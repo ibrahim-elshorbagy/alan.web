264a72f6f529c2ed951c98bde86ee36b2973df36
ibrahim elshorbagy <e.mohmed55@gmail.com>
2026-01-24 14:41:58 +0200

    reorder

--- resources/views/admin/redirect_links/fields_create.blade.php
+++ resources/views/admin/redirect_links/fields_create.blade.php
@@ -1,20 +1,20 @@
 <div class="row">
   <div class="col-lg-6">
     <div class="mb-5">
-      {{ Form::label('redirect_link_type', __('messages.redirect_links.redirect_link_type') . ':', ['class' => 'form-label required']) }}
-      {{ Form::select('redirect_link_type', collect(\App\Enums\RedirectLinkTypeEnum::cases())->mapWithKeys(fn($case) => [$case->value => $case->label()])->toArray(), null, ['class' => 'form-control', 'required']) }}
-    </div>
-  </div>
-  <div class="col-lg-6">
-    <div class="mb-5">
       {{ Form::label('nfcs_id', __('messages.redirect_links.nfc') . ':', ['class' => 'form-label required']) }}
-      {{ Form::select('nfcs_id', $nfcs->pluck('name', 'id'), null, ['class' => 'form-control', 'required', 'id' => 'nfcsSelect']) }}
+      {{ Form::select('nfcs_id', $nfcs->sortBy('name')->pluck('name', 'id'), null, ['class' => 'form-control', 'required', 'id' => 'nfcsSelect']) }}
       <div id="nfcPriceInfo" class="mt-2 text-muted" style="display: none;">
         <small>
           <strong>{{ __('messages.admin_price') }}:</strong> <span id="nfcPrice"></span><br>
           <strong>{{ __('messages.common.sales_price') }}:</strong> <span id="nfcSalesPrice"></span>
         </small>
       </div>
+    </div>
+  </div>
+  <div class="col-lg-6">
+    <div class="mb-5">
+      {{ Form::label('redirect_link_type', __('messages.redirect_links.redirect_link_type') . ':', ['class' => 'form-label required']) }}
+      {{ Form::select('redirect_link_type', collect(\App\Enums\RedirectLinkTypeEnum::cases())->mapWithKeys(fn($case) => [$case->value => $case->label()])->toArray(), null, ['class' => 'form-control', 'required']) }}
     </div>
   </div>
   <div class="col-lg-6">
